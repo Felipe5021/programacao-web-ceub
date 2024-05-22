f2c05dccad54f7179abff402bc8bfc8c30c32044
Felipe5021 <felipevieirafranco1@gmail.com>
2024-05-22 19:54:15 -0300

    New new stuff

--- Dever de Programação para Web/js/Memoria.js
+++ Dever de Programação para Web/js/Memoria.js
@@ -3,19 +3,18 @@ const button = document.querySelector('.login__button');
 const form = document.querySelector('.login-form');
 
 const validateInput = ({ target }) => {
-    if (target.value.length > 3) {
-        button.removeAttribute('disabled');
+  if (target.value.length > 3) {
+    button.removeAttribute('disabled');
     return;
-}
+  }
 
-    button.setAttribute('disabled', '');
-
+  button.setAttribute('disabled', '');
 }
 
 const handleSubmit = (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    localStorage.setItem('player', input.value);
+  localStorage.setItem('player', input.value);
     window.location = 'pages/game.html'
 }
 
